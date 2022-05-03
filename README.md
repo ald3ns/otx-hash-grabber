04cffd3d24bf81e3de50c05934199cbe17330d63
Alden Schmidt <alden@aldenschmidt.com>
2022-05-03 14:29:04 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# otx-hash-grabber
-A simple script to grab all of the hashes for a malware family from OTX
+# OTX Family Hash Grabber
+
+A simple little script to download all of the sample hashes from the OTX malware
+family page.
+
+## Installation
+Install with `pip` or whatever your favorite package manager is. There are only
+two dependencies: `argparse` and `rich`.
+
+```bash
+python3 -m pip install -r requirements.txt
+```
+
+## Usage 
+
+```
+usage: otx-hash-grabber.py [-h] [-f FORMAT] [-k KEY] [-o OUTPUT] family
+
+Grab malware family hashes from OTX
+
+positional arguments:
+  family                Specify the malware family you want in the format
+                        [name]:[platform]/[sample]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -f FORMAT, --format FORMAT
+                        Hash output from md5, sha1, sha256, or all
+  -k KEY, --key KEY     You OTX API key
+  -o OUTPUT, --output OUTPUT
+                        Name of your output file
+```
---
+++ otx-hash-grabber.py
@@ -0,0 +1,90 @@
+import requests
+import argparse
+import json
+from rich.progress import track
+
+def setup_parser():
+    '''
+    Setup argparse stuff
+    '''
+    parser = argparse.ArgumentParser(description="Grab malware family hashes from OTX")
+    parser.add_argument("family", type=str, help="Specify the malware family you want in the format [name]:[platform]/[sample]. Ex: TrojanDownloader:Win32/Cutwail")
+    parser.add_argument("-f", "--format", help="Hash output from md5, sha1, sha256, or all")
+    parser.add_argument("-k", "--key", type=str, help="You OTX API key")
+    parser.add_argument("-o", "--output", type=str, help="Name of your output file")
+    return parser
+
+def main():
+
+    # Some important variables
+    url = "https://otx.alienvault.com/otxapi/malware/samples?"
+    key = "3c33ae9be841e6c05eede941020a37f8bca1097917dd5064a15decbabcb09a79"
+
+    # Setup the argument parser
+    parser = setup_parser()
+    args = parser.parse_args()
+
+    # Check if the API key was supplied, if no then use the supplied key
+    if key == "" and args.key != None:
+        headers = {
+            "X-OTX-API-KEY":args.key
+        }
+    elif key != "":
+        headers = {
+            "X-OTX-API-KEY":key
+        }
+    else:
+        print("Missing API key...")
+        exit()
+
+    # These are the URL parameters, change them as you see fit
+    payload = {
+        "family":args.family,
+        "limit":1000,
+        "page":1
+    }
+
+    # Make first request to get the number of samples
+    number = json.loads(requests.get(url, headers=headers, params=payload).text)
+    num_samples = number["count"]
+    print(f"Expecting {num_samples} hashes")
+
+    # Create the output array for writing or printing
+    to_write = []
+
+    # Loop over all of the page numbers
+    for page in track(range(1, (num_samples//1000)+2), description="[green]Downloading..."):
+
+        # Update the page
+        payload["page"] = page
+
+        # Make the updated request and parse to JSON
+        hashes = json.loads(requests.get(url, headers=headers, params=payload).text)
+
+        # Loop over the results
+        for result in hashes["results"]:
+            if args.format == "md5":
+                to_write.append(result['md5'])
+            elif args.format == "sha1":
+                to_write.append(result['sha1'])
+            elif args.format == "sha256":
+                to_write.append(result['sha256'])
+            else:
+                to_write.append(f"{result['md5']} {result['sha1']} {result['sha256']}")
+
+    # Check if we should write to a file if so do that
+    if args.output != None:
+        file = open(args.output, "w")
+        for i in to_write:
+            file.write(i)
+            file.write("\n")
+        file.close()
+    else:
+        for i in to_write:
+            print(i)
+
+    print(f"Successfully wrote hashes to {args.output}")
+
+
+if __name__ == "__main__":
+    main()
